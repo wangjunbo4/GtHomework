7c60c2001ecfd198bfe80d4d7215238a720f561c
Gtylcara <wjbbugfree@outlook.com>
2021-11-12 22:51:42 +0800

    so far so good, move to bed to continue

--- file.h
+++ file.h
@@ -1,14 +1,15 @@
 /*
  * @Author: Gtylcara.
  * @Date: 2021-11-11 23:58:54
- * @LastEditors: Gtylcara.
- * @LastEditTime: 2021-11-12 00:29:00
+ * @LastEditors: Gtylcara
+ * @LastEditTime: 2021-11-12 22:49:07
  */
 
 #ifndef _FILE_OPT_H_
 #define _FILE_OPT_H_
 
 #include <fstream>
+#include <sstream>
 #include <cstddef>
 #include <tuple>
 #include <variant>
@@ -18,15 +19,37 @@
 template <typename... Ts>
 class File
 {
+public:
     static std::tuple<Ts...> read(std::string path);
     static void write(std::string path, Ts... data);
     static void write(std::string path, std::tuple<Ts...> data);
+
+private:
+    static inline char seperator = ' ';
 };
 
 template <typename... Ts>
 std::tuple<Ts...> File<Ts...>::read(std::string path)
 {
-    
+    std::ifstream ifs;
+    ifs.open(path, std::ios::in);
+    while (!ifs.eof())
+    {
+        std::string s;
+        getline(ifs, s);
+        
+        std::istringstream iss(s);
+        std::vector<std::string> sv(sizeof...(Ts));
+        std::tuple<Ts...> t;
+        std::string buffer;
+        
+        while (std::getline(iss, buffer, seperator))
+        {
+            std::cout << buffer;
+            sv.emplace_back(buffer);
+        }
+    }
+    return std::tuple<Ts...>();
 }
 
 template <typename... Ts>
--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
  * @Github: https://github.com/wangjunbo4
  * @Date: 2021-11-10 19:46:50
  * @LastEditors: Gtylcara
- * @LastEditTime: 2021-11-12 19:15:38
+ * @LastEditTime: 2021-11-12 22:45:49
  * @FilePath: \Homework\main.cpp
  */
 
@@ -14,14 +14,15 @@
 
 
 // define the type you need, and you will be more easily to call it
-typedef Node<long long, std::string, std::string, int, int> myNode;
+#define Type long long, std::string, std::string, int, int
 
-auto data2 = new myNode(2, "child1", "class", 12345, 100);
-auto data1 = new myNode(data2, 1, "head", "class", 12345, 100);
 
-auto data3 = new myNode(3, "push_back", "class", 12345, 100);
-auto data4 = new myNode(4, "push_here", "class", 12345, 100);
-auto data5 = new myNode(5, "push_front", "class", 12345, 100);
+auto data2 = new Node<Type>(2, "child1", "class", 12345, 100);
+auto data1 = new Node<Type>(data2, 1, "head", "class", 12345, 100);
+
+auto data3 = new Node<Type>(3, "push_back", "class", 12345, 100);
+auto data4 = new Node<Type>(4, "push_here", "class", 12345, 100);
+auto data5 = new Node<Type>(5, "push_front", "class", 12345, 100);
 auto head = data1;
 using namespace std;
 
@@ -30,14 +31,16 @@ int main()
     // instantiation class Sys
     Sys sys;
 
+    File<Type>::read("./data.txt");
+
     // three kinds of push, all of them are static
-    myNode::push_back(data2, data3);
-    myNode::push_here(data2, data4);
-    head = myNode::push_front(data1, data5);
+    Node<Type>::push_back(data2, data3);
+    Node<Type>::push_here(data2, data4);
+    head = Node<Type>::push_front(data1, data5);
 
     // search No.1 element of the data tuple called "push_here" and print to cmd window
     std::cout << tuple_index(
-        myNode::search(head, 1, "push_here")->getData(),
+        Node<Type>::search(head, 1, "push_here")->getData(),
         0
     ) << "\n";
     
@@ -49,7 +52,7 @@ int main()
 
     // add one option and its function
     sys.addOptions("print all", []{
-        myNode::printAll(head);
+        Node<Type>::printAll(head);
     });
 
     sys.addOptions("exit", []{
